801c335058da411c0a771e7ae402ae936e2257cb
electronicfreak <github@electronicfreak.de>
2017-02-01 16:58:40 +0100

    alles ordentlich einrücken

--- spacestatus.php
+++ spacestatus.php
@@ -18,10 +18,11 @@ if(isset($_GET) || isset($_POST)) {
 }
 
 $projects = [
-    'https://github.com/HackerspaceBielefeld',
-    'https://wiki.hackerspace-bielefeld.de/Kategorie:Projekt',
-];  
-// "sensors" : {
+	'https://github.com/HackerspaceBielefeld',
+	'https://wiki.hackerspace-bielefeld.de/Kategorie:Projekt',
+	'http://luftdaten-bielefeld.de/',
+];
+
 $sensors = [
 	'temperature' => [
 		[
@@ -36,7 +37,7 @@ $sensors = [
 		'unit' => '%',
 		'location' => 'Outside',
 		'name' => "Street"
-    ],
+	],
 	''
 ];
 
@@ -51,44 +52,45 @@ $feeds = [
 	]
 ];
 
-// TODO 'sensors' => $sensors,
 $string = [
-    'api' => '0.13',
-    'space' => 'Hackerspace Bielefeld e.V.',
-    'logo' => 'https://hackerspace-bielefeld.de/spacestatus/hackerspace-bielefeld-logo.gif',
-    'url' => 'https://hackerspace-bielefeld.de',
-    'location' => [
-        'lat' => 52.038224,
-        'lon' => 8.533056,
-        'address' => 'Sudbrackstrasse 42, 33611 Bielefeld, Germany'
-    ],
-    'contact' => [
-        'phone' => '+49-52-1337-322-42',
-        'jabber' => 'hsb@chat.jabber.space.bi',
-        'ml' => 'public@hackerspace-bielefeld.de',
-        'twitter' => '@HackerspaceBI',
-        'email' => 'info@hackerspace-bielefeld.de',
-        'facebook' => 'https://www.facebook.com/HackerspaceBielefeld',
-        'issue_mail' => 'admin@hackerspace-bielefeld.de'
-    ],
-    'state' => [
-        "icon" =>
-            [
-                'open' => 'https://hackerspace-bielefeld.de/spacestatus/hackerspace-bielefeld-open.png',
-                'closed' => 'https://hackerspace-bielefeld.de/spacestatus/hackerspace-bielefeld-closed.png'],
-        'open' => $data['status'],
-        'lastchange' => time(),
-        'message' => "https://hackerspace-bielefeld.de/spacestatus/display.php",
-        'trigger_person' => "infobot"
-    ],
-    'icon' => [
-        'open' => 'https://hackerspace-bielefeld.de/spacestatus/hackerspace-bielefeld-open.gif',
-        'closed' => 'https://hackerspace-bielefeld.de/spacestatus/hackerspace-bielefeld-closed.gif'],
     'open' => $data['status'],
-    'projects' => $projects,
-    'issue_report_channels' => [
-        'email'
-    ],
+	'api' => '0.13',
+	'space' => 'Hackerspace Bielefeld e.V.',
+	'logo' => 'https://hackerspace-bielefeld.de/spacestatus/hackerspace-bielefeld-logo.gif',
+	'url' => 'https://hackerspace-bielefeld.de',
+	'location' => [
+		'lat' => 52.038224,
+		'lon' => 8.533056,
+		'address' => 'Sudbrackstrasse 42, 33611 Bielefeld, Germany'
+	],
+	'contact' => [
+		'phone' => '+49-52-1337-322-42',
+		'jabber' => 'hsb@chat.jabber.space.bi',
+		'ml' => 'public@hackerspace-bielefeld.de',
+		'twitter' => '@HackerspaceBI',
+		'email' => 'info@hackerspace-bielefeld.de',
+		'facebook' => 'https://www.facebook.com/HackerspaceBielefeld',
+		'issue_mail' => 'admin@hackerspace-bielefeld.de'
+	],
+	'state' => [
+		"icon" =>
+			[
+				'open' => 'https://hackerspace-bielefeld.de/spacestatus/hackerspace-bielefeld-open.png',
+				'closed' => 'https://hackerspace-bielefeld.de/spacestatus/hackerspace-bielefeld-closed.png'],
+		'open' => $data['status'],
+		'lastchange' => time(),
+		'message' => "https://hackerspace-bielefeld.de/spacestatus/display.php",
+		'trigger_person' => "infobot"
+	],
+	'icon' => [
+		'open' => 'https://hackerspace-bielefeld.de/spacestatus/hackerspace-bielefeld-open.gif',
+		'closed' => 'https://hackerspace-bielefeld.de/spacestatus/hackerspace-bielefeld-closed.gif'],
+	'sensors' => $sensors,
+	'feeds' => $feeds,
+	'projects' => $projects,
+	'issue_report_channels' => [
+		'email'
+	],
 ];
 
 $jsonOutput = json_encode($string, JSON_PRETTY_PRINT + JSON_UNESCAPED_SLASHES);
