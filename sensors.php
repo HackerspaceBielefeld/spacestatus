2de5e24e7515a3b771ef13738332f3552df3ee10
electronicfreak <github@electronicfreak.de>
2017-02-01 16:00:45 +0100

    sensor db

--- sensors.php
+++ sensors.php
@@ -1,8 +1,15 @@
 <?php
 	//datei einlesen
-	
+	$data = json_decode(file_get_contents('sensors.json'));
 	//daten ändern
-	
+	if(isset($_GET['keys']) && isset($_GET['vals'])) {}
+		$keys = explode(',',$_GET['keys']);
+		$vals = explode(',',$_GET['vals']);
+		foreach($keys as $i=>$k) {
+			$data[$k] = $vals[$i]
+		}
 	//datei abspeichern
-	
+		file_put_contents('sensors.json',json_encode($data));
+	}
+	print_r($data);
 ?>
